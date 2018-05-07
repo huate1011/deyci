d2e096c47e505b024b9dc48e939633410d46dbe1
miao <huate1011@live.com>
2018-05-07 23:32:58 +1000

    fix the form submit bug and trying a new file upload service

--- weixin/server/controllers/register.js
+++ weixin/server/controllers/register.js
@@ -19,11 +19,24 @@ module.exports = async ctx => {
   
   try{
     const res = await mysql(memberTable).insert(member)
-    ctx.body = "success"
+    ctx.response.status = 200
+    ctx.response.body = "注册成功"
   } catch (err) {
     msg = 'Error: ' + JSON.stringify(err)
     console.log(msg)
-    ctx.body = msg
+    ctx.status = 400
+    ctx.type = 'application/json'
+    ctx.body = ''
+    if (err['code'] == 'ER_DUP_ENTRY') {
+      if (err['sqlMessage'].indexOf('phone') > -1) {
+        ctx.body = {error:"电话号码已被使用"}
+      } else if (err['sqlMessage'].indexOf('personalid') > -1) {
+        ctx.body = {error: "身份证号已被使用"}
+      }
+    }
+    if (ctx.body == '') {
+      ctx.body = {error: err['sqlMessage']}
+    }  
   }
   
   
